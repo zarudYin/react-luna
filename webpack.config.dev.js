1f5830384b14a7556b25d2e8ac6c6f32c40f462c
zarudYin <715376377@qq.com>
2017-03-31 00:04:57 +0800

    CommonsChunkPlugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -49,6 +49,9 @@ module.exports = function (env) {
             new HtmlWebpackPlugin({
                 template: './index.html'
             }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor',
+            }),
             // 开启全局的模块热替换（HMR）
             new webpack.HotModuleReplacementPlugin(),
             // 当模块热替换（HMR）时在浏览器控制台输出对用户更友好的模块名字信息
